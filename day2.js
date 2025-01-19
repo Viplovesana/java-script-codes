2e303c9306c3979c328d9635e9978979f44c0eea
Viplovesana <viplovesana90@gmail.com>
2025-01-20 04:28:58 +0530

    initial commit

---
+++ day2.js
@@ -0,0 +1,100 @@
+ 
+ function okk(){
+
+    console.log("okk");
+
+ }
+  okk();
+
+  function start(){
+
+    console.log("hello world")
+
+  }
+   start();
+  // .................homework................//
+
+
+  start();
+
+  function num(){
+
+    var a=7;
+    console.log(a)
+  }
+  num();
+
+  function starts(){
+    var a=5;
+    var b=8;
+    console.log(a+b);
+  }
+  starts();
+
+  function okk1(){
+    var a=6;
+    var b=9;
+    var c=10;
+    console.log(a-b-c);
+  }
+  okk1();
+  
+  function okk2(){
+    var a=6;
+    var b=9;
+    var c=10;
+    console.log(a*b*c);
+  }
+  okk2();
+  
+  
+  function okk3(){
+    var a=6;
+    var b=9;
+    var c=10;
+    console.log(a/b/c);
+  }
+  okk3();
+
+  
+  function okk4(){
+    var a=6;
+    var b=9;
+    console.log(a%b);
+  }
+  okk4();
+
+  //let & const used after update in 2015../
+
+  function go(){
+     
+    //declare
+    // var a=4;
+    // var a=7;
+    // console.log(a);
+
+
+    //........let and const is a block scope.......//
+
+    // let a=20;
+    // {
+    //   let a=25;
+    //   console.log("inner"+a);
+    // }
+    // console.log("outer"+a);
+
+
+    //hoisting...................
+
+      // console.log(a);
+
+      // var a=3;//var support the hoisting 
+
+      // let a=5;//let and const not support the hoisting
+
+
+
+
+  }
+
+go();
---
+++ string.js
@@ -0,0 +1,29 @@
+
+
+
+
+function practice(){
+
+let a="this is string"
+let a2="this is my second string"  //string
+// let a2="this is  second string"
+// let a3="this is my third string"
+
+//length
+// document.write(a.length) ///return the lenght of string..
+
+
+//concatination..
+// document.write(a.concat(a2,a3))//combine 2 or more string in one
+
+//upper and lower case
+// document.write(a.toUpperCase())
+// document.write(a2.toLocaleLowerCase())
+
+//slice..
+// document.write(a2.slice(3,9))
+
+//homework.trim ..charat..charcodeat...split....indexof()
+
+
+}
