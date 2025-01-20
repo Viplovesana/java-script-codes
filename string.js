c95eae126d56308247c2ced699e7c18570d4b28e
Viplovesana <viplovesana90@gmail.com>
2025-01-21 02:44:34 +0530

    new commit

--- string.js
+++ string.js
@@ -2,10 +2,10 @@
 
 
 
-function practice(){
+// function practice(){
 
-let a="this is string"
-let a2="this is my second string"  //string
+// let a="this is string"
+// let a2="this is my second string"  //string
 // let a2="this is  second string"
 // let a3="this is my third string"
 
@@ -26,4 +26,53 @@ let a2="this is my second string"  //string
 //homework.trim ..charat..charcodeat...split....indexof()
 
 
+// }
+
+function practice(){
+
+    let a = "this is my string paragraph"
+    let b = "this&is&my&second&string&paragraph"
+    let c = "this is my third string paragraph"
+
+    //length...
+    // document.write(a2.length)
+
+    //concatination.......
+    // document.write(a.concat(b,c)) //addition of one or two 
+
+    //slice.....
+    // document.write(a.slice(3,9)) //indexing no
+
+    //uppercase and lower case.........
+    // document.write(a.toUpperCase())
+    // document.write(b.toLowerCase())
+
+    //trim...
+    // console.log(a)//it can trim extra white space the start and end words...
+
+    //charat....
+    // document.write(a.charAt(4))//it can print particular character by the no..
+
+    ///charcodeat....
+    
+    // document.write(b.charCodeAt(3))//charcodeat print the unique no code of the specific word..
+
+    //split method...
+    //split method breaks the string up into separate string and assingn into an array
+    // console.log(b.split("&"))
+    // document.write(arr[5]);
+
+    //indexof method......
+    // document.write(a.indexOf( ))
+
+    //replace....
+    let abc="Cybrom is located at cybrom and Cybrom is good"
+    // document.write(abc.replace("Cybrom","MP NAGAR"))       
+    // document.write(abc.replace(/Cybrom/gi,"MP NAGAR"))   //g = global //i = case sensitve   
+
+    
+
+
+
 }
+
