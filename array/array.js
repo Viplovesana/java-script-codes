5fb2e4dea8b4f16b8cb8551268ec16bebcd5a1ce
Viplovesana <viplovesana90@gmail.com>
2025-01-23 11:37:12 +0530

    new commit

--- array/array.js
+++ array/array.js
@@ -4,7 +4,28 @@ let fruits=["banaana","mango","apple","papaya"]
 
 // document.write(fruits.length);
 
-fruits.pop()
-document.write(fruits)//pop removes the last element
+// fruits.pop()
+// document.write(fruits)//pop removes the last element
 
-fruits.shift()
+// fruits.shift()
+// document.write(fruits)
+
+//slice method......
+// document.write(fruits.slice(1,3))
+
+//splice method......
+// fruits.splice(1,2,"grapes")
+// document.write(fruits)
+
+//reverse array method..........
+// fruits.reverse()
+// document.write(fruits)
+
+
+//assending and dessending order method....
+
+// let abc=[2,6,1,8,4,9,7]
+// document.write(abc.sort())//assending..
+
+// let abc=[2,6,1,8,4,9,7]
+// document.write(abc.sort().reverse())//dessending..
