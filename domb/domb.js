81c474e315f83109c795cb2438533186d74ba77c
Viplovesana <viplovesana90@gmail.com>
2025-01-25 11:54:06 +0530

    new commit

--- domb/domb.js
+++ domb/domb.js
@@ -1,11 +1,49 @@
+// // what is domb ?
+
+// // it is tree like structure of an html where js can enter and can change and manuplate any content or tag....
+
+// function common(){
+
+// let show = document.querySelector('#div')
 
 
-function common(){
 
-let show = document.querySelector('#name')
+// show.style.backgroundImage="url(camera.jpeg)"
 
-show.innerHTML="viplove sana"
+// }
 
-show.style.color="red"
+// function vip(){
+//     let show = document.querySelector('#div')
+
+//     show.style.backgroundColor="red"
+
+
+// }
+
+// function rest(){
+
+//     let show = document.querySelector('#div')
+
+//       show.style.backgroundColor="green"
+//       show.style.backgroundImage="none";
+
+     
+    
+// }
+
+function que(){
+
+    let box1 = document.querySelector('#box1')
+    let box2 = document.querySelector('#box2')
+    let box3 = document.querySelector('#box3')
+
+    box1.style.backgroundColor='orange'
+       box2.style.backgroundColor='white'
+          box3.style.backgroundColor='green'
+
+          box1.style.display='none'
+            box2.style.display='none'
+              box3.style.display='none'
+
 
 }
