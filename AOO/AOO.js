a65441b161771a195caa1c812c1b13be868961f9
Viplovesana <viplovesana90@gmail.com>
2025-02-04 11:50:23 +0530

    new commit

--- AOO/AOO.js
+++ AOO/AOO.js
@@ -1,57 +1,76 @@
 //...........ARRAY OF OBJECT..............//
 
 
-// let Aoo = [{
+let students = [{
 
-//     name:"sid",
-//     age:25,
-//     city:"bhopal"
-// },{
-//     name:"faraz",
-//     age:24,
-//     city:"jabalpur"
-// },{
-//     name:"annanya",
-//     age:21,
-//     city:"indore"
-// },{
-//     name:"viplove",
-//     age:23,
-//     city:"bhopal"
-// }];
+    name:"sid",
+    age:25,
+    city:"bhopal"
+},{
+    name:"faraz",
+    age:24,
+    city:"jabalpur"
+},{
+    name:"annanya",
+    age:21,
+    city:"indore"
+},{
+    name:"viplove",
+    age:23,
+    city:"bhopal"
+}];
 
-
-//MAP, FILTER, FOREACH .......................//
-
-
-//........MAP......//
-
-let arr1 = [4,7,8,9,4,9]
-
-let ans = arr1.map((event)=>{
-
-    return event*2
-})
-
-console.log(ans);
+let display = document.querySelector("#datadisplay")
 
 
-//....filter...............//
 
-let ans2 = arr1.filter((e)=>{
+students.map((e)=>{
 
-    return e%2==0
-})
-console.log(ans2)
+display.innerHTML+=`
+<div>
+<h1 id="name"> name:- ${e.name}</h1>
+<h2>age:- ${e.age}</h2>
+<h2>city:-${e.city}</h2>
+</div>
 
-//........foreach,,,///
+`
+}
+)
 
- arr1.forEach((e)=>{
 
-    console.log(e%2==0)
+// //MAP, FILTER, FOREACH .......................//
+
+
+// //........MAP......//
+
+// let arr1 = [4,7,8,9,4,9]
+
+// let ans = arr1.map((event)=>{
+
+//     return event*2
+// })
+
+// console.log(ans);
+
+
+// //....filter...............//
+
+// let ans2 = arr1.filter((e)=>{
+
+//     return e%2==0
+// })
+// console.log(ans2)
+
+// //........foreach,,,///
+
+//  arr1.forEach((e)=>{
+
+//     console.log(e%2==0)
 
   
-})
+// })
+
+
 
 
 
